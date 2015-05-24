84ff9c0d9269a1e64d2ece7edcad8c54421814b1
Nick Gauthier <ngauthier@gmail.com>
2015-05-24 11:45:53 -0400

    basic examples

--- basics/package_test.go
+++ basics/package_test.go
@@ -1,9 +1,29 @@
 package basics
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
+)
 
 func TestTruth(t *testing.T) {
 	if true != true {
 		t.Error("everything I know is wrong")
+	}
+}
+
+func TestBroken(t *testing.T) {
+	if true != false {
+		t.Error("expected", true, "got", false)
+	}
+
+	if 1+1 != 4 {
+		t.Fatal("Can't proceed!", 1+1, 4)
+	}
+}
+
+func TestError(t *testing.T) {
+	dest := make([]byte, 0)
+	if _, err := hex.Decode(dest, []byte{8}); err != nil {
+		t.Error(err)
 	}
 }
